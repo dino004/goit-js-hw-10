23bd36f937f265d92c45f3e1f83d28dd233bc522
Dmytro Lytvyn <dmytro4lytvyn@gmail.com>
2023-05-16 22:09:26 +0300

    Додав слухача подій на інпут

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+var debounce = require('lodash.debounce');
+
 
 const DEBOUNCE_DELAY = 300;
+let coutryInput;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(evt) {
+  coutryInput = evt.target.value;
+  console.log(coutryInput);
+}
+
+function fetchCountries(coutry) {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+}
+
+return fetch('${BASE_URL}/alpha/${countryInput}.json').then(resp => {
+  if (!resp.ok) {
+    throw new Error(resp.statusText);
+  }
+
+  return resp.json();
+});
